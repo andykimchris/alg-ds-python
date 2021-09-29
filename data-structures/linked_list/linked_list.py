ea098a6a3aa3aa50fce4fb3d6e171ea1c0f00c2c
andykimchris <andrew.muchiri97@gmail.com>
2021-09-29 16:25:28 +0300

    length of linked list '

--- data-structures/linked_list/linked_list.py
+++ data-structures/linked_list/linked_list.py
@@ -8,6 +8,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+
+        return count
+
     def print(self):
         if self.head is None:
             print("Linked list is empty")
