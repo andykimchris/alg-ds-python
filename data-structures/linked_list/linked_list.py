0b63d0419afc386f65abe29152839a1c0338a468
andykimchris <andrew.muchiri97@gmail.com>
2021-09-29 16:31:17 +0300

    removed node at a certain index

--- data-structures/linked_list/linked_list.py
+++ data-structures/linked_list/linked_list.py
@@ -68,3 +68,21 @@ class LinkedList:
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count+=1
