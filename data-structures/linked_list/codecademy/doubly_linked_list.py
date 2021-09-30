9042b035bab20af749e3b094fe5d78e19307540a
andykimchris <andrew.muchiri97@gmail.com>
2021-09-30 18:48:48 +0300

    implemented removed_head & removed_tail of nodes

--- data-structures/linked_list/codecademy/doubly_linked_list.py
+++ data-structures/linked_list/codecademy/doubly_linked_list.py
@@ -51,6 +51,38 @@ class DoublyLinkedList:
     if self.head_node == None:
       self.head_node = new_tail
 
+  def remove_head(self):
+    removed_head = self.head_node
+
+    if removed_head == None:
+      return None
+
+    self.head_node = removed_head.get_next_node()
+
+    if self.head_node != None:
+      self.head_node.set_prev_node(None)
+
+    if removed_head == self.tail_node:
+      self.remove_tail()
+
+    return removed_head.get_value()
+
+  def remove_tail(self):
+    removed_tail = self.tail_node
+
+    if removed_tail == None:
+      return None
+
+    self.tail_node = removed_tail.get_prev_node()
+
+    if self.tail_node != None:
+      self.tail_node.set_next_node(None)
+
+    if removed_tail == self.head_node:
+      self.remove_head()
+
+    return removed_tail.get_value()
+
 
 
   def stringify_list(self):
