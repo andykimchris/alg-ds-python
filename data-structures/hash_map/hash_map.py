a2b5e31545edad7a4138babe6f242ee90b170a37
andykimchris <andrew.muchiri97@gmail.com>
2021-10-01 17:53:59 +0300

    assigned keys with values to hash map

--- data-structures/hash_map/hash_map.py
+++ data-structures/hash_map/hash_map.py
@@ -11,14 +11,44 @@ class HashMap:
     def compressor(self, hash_code):
         return hash_code % self.array_size
 
-    
+    def assign(self, key, value):
+        array_index = self.compressor(self.hash(key))
+        current_array_value = self.array[array_index]
+
+        if current_array_value is None:
+            self.array[array_index] = [key, value]
+            return
+
+        if current_array_value[0] == key:
+            self.array[array_index] = [key, value]
+            return
+
+        # Collision!
+        number_collisions = 1
+
+        while(current_array_value[0] != key):
+            new_hash_code = self.hash(key, number_collisions)
+            new_array_index = self.compressor(new_hash_code)
+            current_array_value = self.array[new_array_index]
+
+            if current_array_value is None:
+              self.array[new_array_index] = [key, value]
+              return
+
+            if current_array_value[0] == key:
+              self.array[new_array_index] = [key, value]
+              return
+
+            number_collisions += 1
+
+        return
 
 
 
-# hash_map = HashMap(15)
-# hash_map.assign('gabbro','igneous')
-# hash_map.assign('sandstone','sedimentary')
-# hash_map.assign('gneiss','metamorphic')
+hash_map = HashMap(15)
+hash_map.assign('gabbro','igneous')
+hash_map.assign('sandstone','sedimentary')
+hash_map.assign('gneiss','metamorphic')
 
 # print(hash_map.retrieve('gabbro'))
 # print(hash_map.retrieve('sandstone'))
