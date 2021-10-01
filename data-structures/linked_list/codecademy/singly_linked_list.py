ed93292d09a9bf33be5ecdbe6ad643e7459d09cd
andykimchris <andrew.muchiri97@gmail.com>
2021-10-01 17:47:49 +0300

    tested methods

--- data-structures/linked_list/codecademy/singly_linked_list.py
+++ data-structures/linked_list/codecademy/singly_linked_list.py
@@ -1,30 +1,30 @@
 class Node:
-  def __init__(self, value, next_node=None):
+    def __init__(self, value, next_node=None):
     self.value = value
     self.next_node = next_node
 
-  def get_value(self):
+    def get_value(self):
     return self.value
 
-  def get_next_node(self):
+    def get_next_node(self):
     return self.next_node
 
-  def set_next_node(self, next_node):
+    def set_next_node(self, next_node):
     self.next_node = next_node
 
 class LinkedList:
-  def __init__(self, value=None):
+    def __init__(self, value=None):
     self.head_node = Node(value)
 
-  def get_head_node(self):
+    def get_head_node(self):
     return self.head_node
 
-  def insert_beginning(self, new_value):
+    def insert_beginning(self, new_value):
     new_node = Node(new_value)
     new_node.set_next_node(self.head_node)
     self.head_node = new_node
 
-  def stringify_list(self):
+    def stringify_list(self):
     string_list = ""
     current_node = self.get_head_node()
     while current_node:
@@ -33,7 +33,7 @@ class LinkedList:
       current_node = current_node.get_next_node()
     return string_list
 
-  def remove_node(self, value_to_remove):
+    def remove_node(self, value_to_remove):
     current_node = self.get_head_node()
     if current_node.get_value() == value_to_remove:
       self.head_node = current_node.get_next_node()
@@ -45,3 +45,22 @@ class LinkedList:
           current_node = None
         else:
           current_node = next_node
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(["volvo","nissan","subaru","toyota","honda","ram"])
+    print("Initial linked list")
+    ll.print()
+
+    ll.insert_at(1,"audi")
+    ll.remove_at(2)
+    print("Insert and remove")
+    ll.print()
+
+    ll.remove_node_by_value('toyota')
+    print("Remove node by value: toyota")
+    ll.print()
+
+    print("Insert at end: kia")
+    ll.insert_at_end('kia')
+    ll.print()
