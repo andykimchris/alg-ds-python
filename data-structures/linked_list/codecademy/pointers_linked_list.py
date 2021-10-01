f1ced68dfaf1b46c5ad6d7fd66d9e62a30cd3a22
andykimchris <andrew.muchiri97@gmail.com>
2021-10-01 17:49:09 +0300

    find middle node of linked list

--- data-structures/linked_list/codecademy/pointers_linked_list.py
+++ data-structures/linked_list/codecademy/pointers_linked_list.py
@@ -11,3 +11,14 @@ def nth_last_node(linked_list, n):
       else:
         current = current.get_next_node()
     return current
+
+
+ def find_middle(linked_list):
+    fast = linked_list.head_node
+    slow = linked_list.head_node
+    while fast:
+        fast = fast.get_next_node()
+    if fast:
+        fast = fast.get_next_node()
+        slow = slow.get_next_node()
+    return slow
