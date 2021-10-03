de33dbb777ca8e27b9e68bd7f5cffe1b6dba0504
andykimchris <andrew.muchiri97@gmail.com>
2021-10-03 18:24:37 +0300

    add values to max heap list

--- data-structures/heaps/maxheaps.py
+++ data-structures/heaps/maxheaps.py
@@ -11,3 +11,9 @@ class MaxHeap:
 
   def right_child_idx(self, idx):
     return idx * 2 + 1
+
+  def add(self, element):
+    self.count += 1
+    print("Adding: {0} to {1}".format(element, self.heap_list))
+    self.heap_list.append(element)
+    self.heapify_up()
