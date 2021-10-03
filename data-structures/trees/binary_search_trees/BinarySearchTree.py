ba031af3fc7e755b42409122c965df2fb7032021
andykimchris <andrew.muchiri97@gmail.com>
2021-10-03 18:07:48 +0300

    in order traversal(DFS)-left,root,right

--- data-structures/trees/binary_search_trees/BinarySearchTree.py
+++ data-structures/trees/binary_search_trees/BinarySearchTree.py
@@ -20,6 +20,7 @@ class BinarySearchTree:
         print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
       else:
         self.right.insert(value)
+        
   def get_node_by_value(self, value):
     if (self.value == value):
       return self
@@ -29,3 +30,20 @@ class BinarySearchTree:
       return self.right.get_node_by_value(value)
     else:
       return None
+  
+  def depth_first_traversal(self):
+    if (self.left is not None):
+      self.left.depth_first_traversal()
+    print(f'Depth={self.depth}, Value={self.value}')
+    if (self.right is not None):
+      self.right.depth_first_traversal()
+
+
+print("Creating Binary Search Tree rooted at value 15:")
+tree = BinarySearchTree(15)
+
+for x in range(10):
+  tree.insert(random.randint(0, 100))
+  
+print("Printing the inorder depth-first traversal:")
+tree.depth_first_traversal()
