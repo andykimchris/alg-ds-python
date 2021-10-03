75ff4dbe6d3c3ee197cae0cef640868e2fc8ef32
andykimchris <andrew.muchiri97@gmail.com>
2021-10-03 18:20:21 +0300

    helper methods to find child & parent indexes

--- data-structures/heaps/maxheaps.py
+++ data-structures/heaps/maxheaps.py
@@ -2,3 +2,12 @@ class MaxHeap:
   def __init__(self):
     self.heap_list = [None]
     self.count = 0
+
+  def parent_idx(self, idx):
+    return idx // 2
+
+  def left_child_idx(self, idx):
+    return idx * 2
+
+  def right_child_idx(self, idx):
+    return idx * 2 + 1
