c1c34dba5d6f5b61e172120409ebd4a3ed2bab5d
andykimchris <andrew.muchiri97@gmail.com>
2021-10-03 18:39:02 +0300

    heapify up by swapping elements if parent is smaller than child

--- data-structures/heaps/maxheaps.py
+++ data-structures/heaps/maxheaps.py
@@ -17,3 +17,16 @@ class MaxHeap:
     print("Adding: {0} to {1}".format(element, self.heap_list))
     self.heap_list.append(element)
     self.heapify_up()
+
+  def heapify_up(self):
+    print("Heapifying up")
+    idx = self.count
+    while self.parent_idx(idx) > 0:
+      child = self.heap_list[idx]
+      parent = self.heap_list[self.parent_idx(idx)]
+      if parent < child:
+        print("swapping {0} with {1}".format(parent, child))
+        self.heap_list[idx] = parent
+        self.heap_list[self.parent_idx(idx)] = child
+      idx = self.parent_idx(idx)
+    print("Heap Restored {0}".format(self.heap_list))
