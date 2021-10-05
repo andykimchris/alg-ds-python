bfe6e608272b28a29a6ec2f92c5202ee119dfc37
andykimchris <andrew.muchiri97@gmail.com>
2021-10-05 20:25:53 +0300

    added edges between vertices

--- data-structures/graphs/graph.py
+++ data-structures/graphs/graph.py
@@ -6,4 +6,7 @@ class Graph:
   def add_vertex(self, vertex):
     self.graph_dict[vertex.value] = vertex
 
-    
+  def add_edge(self, from_vertex, to_vertex, weight = 0):
+    self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+    if not self.directed:
+      self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
