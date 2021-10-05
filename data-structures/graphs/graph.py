02ea683c1536c60fdbc83c83fa654f848e372c94
andykimchris <andrew.muchiri97@gmail.com>
2021-10-05 20:24:01 +0300

    added vertex to dictionary using vertex value as key

--- data-structures/graphs/graph.py
+++ data-structures/graphs/graph.py
@@ -2,3 +2,8 @@ class Graph:
   def __init__(self, directed = False):
     self.graph_dict = {}
     self.directed = directed
+
+  def add_vertex(self, vertex):
+    self.graph_dict[vertex.value] = vertex
+
+    
