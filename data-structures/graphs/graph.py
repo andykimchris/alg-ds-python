5c0c8294658a1b49f8b2e4072b0c27476ba9322e
andykimchris <andrew.muchiri97@gmail.com>
2021-10-05 20:43:25 +0300

    find path btwn nodes

--- data-structures/graphs/graph.py
+++ data-structures/graphs/graph.py
@@ -8,5 +8,23 @@ class Graph:
 
   def add_edge(self, from_vertex, to_vertex, weight = 0):
     self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+    # for bi-directional graph
     if not self.directed:
       self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+  def find_path(self, start_vertex, end_vertex):
+    start = [start_vertex]
+    seen = {}
+    while len(start) > 0:
+      current_vertex = start.pop(0)
+      # update visited vertex to seen
+      seen[current_vertex] = True
+      print("Visiting " + current_vertex)
+      if current_vertex == end_vertex:
+        return True
+      else:
+        # get edges of current vertex as nodes to visit
+        vertices_to_visit = set(self.graph_dict[current_vertex].edges.keys())
+        # visit nodes NOT in seen
+        start += [vertex for vertex in vertices_to_visit if vertex not in seen]
+    return False
