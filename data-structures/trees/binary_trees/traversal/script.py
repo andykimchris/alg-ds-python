fc710571f36e7b820cc16b78796cbec52d2e42b0
andykimchris <andrew.muchiri97@gmail.com>
2021-10-13 23:40:22 +0300

    pre order tree traversal

---
+++ data-structures/trees/binary_trees/traversal/script.py
@@ -0,0 +1,28 @@
+def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return None
+
+        result = []
+        if root.left:
+            result = self.inorderTraversal(root.left)
+
+        result.append(root.val)
+
+        if root.right:
+            result = result + self.inorderTraversal(root.right)
+
+        return result
+
+def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return None
+
+        result = []
+        result.append(root.val)
+
+        if root.left:
+            result = result + self.preorderTraversal(root.left)
+        if root.right:
+            result = result + self.preorderTraversal(root.right)
+
+        return result
