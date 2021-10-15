30c3f5600462979e56e6f3fdc053a2bc5a364fb3
andykimchris <andrew.muchiri97@gmail.com>
2021-10-15 18:50:15 +0300

    merged two binary trees together

--- data-structures/trees/binary_trees/problems/script.py
+++ data-structures/trees/binary_trees/problems/script.py
@@ -8,3 +8,16 @@ def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         self.invertTree(root.right)
 
         return root
+
+def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        # Either merge root1 to root2 or vice-versa, both work
+        root2.val += root1.val
+        root2.left = self.mergeTrees(root1.left, root2.left)
+        root2.right = self.mergeTrees(root1.right, root2.right)
+
+        return root2
