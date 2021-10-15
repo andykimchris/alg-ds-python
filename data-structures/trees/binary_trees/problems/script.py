d7351cb9f2a059bbc4eacf895d3981bf8b1967b8
andykimchris <andrew.muchiri97@gmail.com>
2021-10-15 18:48:11 +0300

    invert a binary tree

---
+++ data-structures/trees/binary_trees/problems/script.py
@@ -0,0 +1,10 @@
+def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
