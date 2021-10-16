c3b5cb24df65d92cd395fa4fb5a16c47d2b4c59c
andykimchris <andrew.muchiri97@gmail.com>
2021-10-16 19:12:18 +0300

    post-order traversal of n-ary tree

--- data-structures/trees/n-ary trees/traversal/script.py
+++ data-structures/trees/n-ary trees/traversal/script.py
@@ -9,3 +9,14 @@ def preorder(self, root: 'Node') -> List[int]:
             res += self.preorder(child)
 
         return res
+        
+def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return None
+
+        res = []
+        for child in root.children:
+            res += self.postorder(child)
+
+        res.append(root.val)
+        return res
