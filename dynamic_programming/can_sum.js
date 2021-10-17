98a6119ff066f483b47265b7143a2a8086caba58
andykimchris <andrew.muchiri97@gmail.com>
2021-10-17 11:44:53 +0300

    memoized canSum

---
+++ dynamic_programming/can_sum.js
@@ -0,0 +1,32 @@
+/*
+Write a fn 'canSum(targetSum, numbers)' that should return a boolean indicating whether or not it is possible
+to generate the targetSum using numbers from the array.
+
+You may use an element from the array as many times as needed
+You may assume all numbers are positive integers.
+
+*/
+ const canSum = (targetSum, numbers, memo={}) => {
+   if (targetSum in memo) return memo[targetSum]
+
+   if (targetSum === 0) return true
+   if (targetSum < 0) return false
+
+   for (let num of numbers) {
+     const remainder = targetSum - num
+     if (canSum(remainder, numbers, memo) === true) {
+       memo[targetSum] = true
+       return true
+     }
+   }
+
+  memo[targetSum] = false
+  return false
+ }
+
+console.log(canSum(7, [2,3])) // true
+console.log(canSum(7, [5,3,4,7])) // true
+console.log(canSum(7, [2,4])) // false
+console.log(canSum(8, [2,3,5])) //true
+
+console.log(canSum(300, [7,14])) //false
