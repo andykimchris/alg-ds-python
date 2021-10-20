ec20b1413b7043217062e7e679ba08c1eb6bc7e0
andykimchris <andrew.muchiri97@gmail.com>
2021-10-20 21:10:05 +0300

    iterative grid traveller using tabulation

--- dynamic_programming/grid_traveller.js
+++ dynamic_programming/grid_traveller.js
@@ -29,10 +29,30 @@ const gridTraveler = (m, n, memo={}) => {
 }
 
 console.log(gridTraveler(1,1)) // 1
-console.log(gridTraveler(3,3)) // 6
 
 // order or rows and columns doesn't matter
-console.log(gridTraveler(2,3)) // 3
 console.log(gridTraveler(3,2)) // 3
 
 console.log(gridTraveler(18,18)) // 2333606220
+
+
+const grid_traveler = (m,n) => {
+  const table = Array(m + 1).fill().map(() => Array(n + 1).fill(0))
+  table[1][1] = 1
+
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      const current = table[i][j]
+      if (j+1 <= n) table[i][j + 1] += current
+      if (i+1 <= m) table[i + 1][j] += current
+    }
+  }
+  return table[m][n]
+}
+
+console.log(grid_traveler(2,2)) // 2
+// order or rows and columns doesn't matter
+console.log(grid_traveler(4,5)) // 35
+console.log(grid_traveler(5,4)) // 35
+
+console.log(grid_traveler(20,20)) // 35345263800
