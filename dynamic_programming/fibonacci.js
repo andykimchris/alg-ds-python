c2a3cc2199ee3dc8c89a960795de17c9103b1dfc
andykimchris <andrew.muchiri97@gmail.com>
2021-10-20 18:37:52 +0300

    iterative path to fib using prefilled array

--- dynamic_programming/fibonacci.js
+++ dynamic_programming/fibonacci.js
@@ -1,6 +1,7 @@
 // memoization
 // js object, keys will be args to fn, value will be the return value
 
+// recursion
 const fib = (n, memo = {}) => {
   if (n in memo) {
     return memo[n]
@@ -13,7 +14,23 @@ const fib = (n, memo = {}) => {
   memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
   return memo[n]
 }
-console.log(fib(5))
-console.log(fib(7))
-console.log(fib(8))
- console.log(fib(50))
+
+console.log(fib(5)) // 5
+console.log(fib(6)) // 8
+console.log(fib(40)) // 102334155
+
+// iterative
+const fibonacci = n => {
+  const table = Array(n+1).fill(0)
+  table[1] = 1
+
+  for (var i = 0; i <= n; i++) {
+    table[i + 1] += table[i]
+    table[i + 2] += table[i]
+  }
+  return table[n]
+}
+
+console.log(fibonacci(7)) // 13
+console.log(fibonacci(8)) // 21
+console.log(fibonacci(50)) // 12586269025
