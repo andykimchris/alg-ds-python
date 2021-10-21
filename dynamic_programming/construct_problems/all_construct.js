c011527b35e546f64da47276e5a8b7dac688f2e6
andykimchris <andrew.muchiri97@gmail.com>
2021-10-21 17:57:52 +0300

    iterative all construct

--- dynamic_programming/construct_problems/all_construct.js
+++ dynamic_programming/construct_problems/all_construct.js
@@ -34,3 +34,30 @@ console.log(allConstruct('purple',['purp','p','ur', 'le', 'purpl']))
 // [ [ 'purp', 'le' ], [ 'p', 'ur', 'p', 'le' ] ]
 console.log(allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','ee','eeeee']))
 // []
+
+const all_construct = (target, wordBank) => {
+  const table = Array(target.length + 1)
+      .fill()
+      .map(() => [])
+  table[0] = [[]]
+
+  for (var i = 0; i < table.length; i++) {
+    for (var word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        const newCombinations = table[i].map(subArray => [...subArray, word])
+        table[i + word.length].push(...newCombinations)
+      }
+    }
+  }
+  return table[target.length]
+}
+
+console.log('iterative');
+console.log(all_construct('abcdef', ['ab','abc','cd','def','abcd']))
+// [ [ 'abc', 'def' ] ]
+console.log(all_construct('skateboard',['bo','rd','ate','t','ska','sk','boar']))
+// []
+console.log(all_construct('purple',['purp','p','ur', 'le', 'purpl']))
+// [ [ 'purp', 'le' ], [ 'p', 'ur', 'p', 'le' ] ]
+console.log(all_construct('eeeeeeeeeeeeef', ['e','ee','eeeee']))
+// []
