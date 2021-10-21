d4ed4c64ae4f12cd0a2c7a98ebd0a99e70753c03
andykimchris <andrew.muchiri97@gmail.com>
2021-10-21 14:27:05 +0300

    iterative can construct

--- dynamic_programming/construct_problems/can_construct.js
+++ dynamic_programming/construct_problems/can_construct.js
@@ -29,3 +29,26 @@ console.log(canConstruct('abcdef', ['ab','abc','cd','def','abcd'])) // true
 console.log(canConstruct('skateboard',['bo','rd','ate','t','ska','sk','boar'])) // false
 console.log(canConstruct('menu',['m','en','nu'])) // false
 console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','ee','eeeee','f'])) //true
+
+const can_construct = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(false)
+  table[0] = true
+
+  for (var i = 0; i < target.length; i++) {
+    if (table[i]) {
+      for (var word of wordBank) {
+        // if the word matches the characters starting at position 1
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] = true
+        }
+      }
+    }
+  }
+  return table[target.length]
+}
+
+console.log('iterative solution');
+console.log(can_construct('abcdef', ['ab','abc','cd','def','abcd'])) // true
+console.log(can_construct('skateboard',['bo','rd','ate','t','ska','sk','boar'])) // false
+console.log(can_construct('menu',['m','en','nu'])) // false
+console.log(can_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','ee','eeeee'])) // false
