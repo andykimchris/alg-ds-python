74525db841ab668167c147d0d652c0b20fa2d2ab
andykimchris <andrew.muchiri97@gmail.com>
2021-10-21 13:39:11 +0300

    iterative best sum

--- dynamic_programming/sum_problems/best_sum.js
+++ dynamic_programming/sum_problems/best_sum.js
@@ -44,3 +44,28 @@ console.log(bestSum(7, [5,3,4,7])) // [7]
 console.log(bestSum(8, [2,3,5])) // [3,5]
 console.log(bestSum(8, [1,4,5])) // [4,4]
 console.log(bestSum(100, [1,2,5,25])) // [25,25,25,25]
+
+
+const how_sum = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(null)
+  table[0] = []
+
+  for (var i = 0; i <= targetSum; i++) {
+    if (table[i] !== null) {
+        for (var num of numbers) {
+          const combination = [ ...table[i], num ]
+          // check table[i + num] is not null
+          if (!table[i + num] || table[i + num].length > combination.length) {
+            table[i + num] = combination
+          }
+        }
+    }
+  }
+  return table[targetSum]
+}
+
+console.log('iterative solution');
+console.log(how_sum(6, [1,2,4])) // [2,4]
+console.log(how_sum(6, [3,4])) // [3,3]
+console.log(how_sum(12, [2,4,5])) // [2,5,5]
+console.log(how_sum(100, [7,14])) // null
