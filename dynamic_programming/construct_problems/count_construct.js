45795e8a21133d92e6cbf3e710c71c92483e29e0
andykimchris <andrew.muchiri97@gmail.com>
2021-10-21 17:27:27 +0300

    iterative count construct

--- dynamic_programming/construct_problems/count_construct.js
+++ dynamic_programming/construct_problems/count_construct.js
@@ -28,3 +28,28 @@ console.log(countConstruct('abcdef', ['ab','abc','cd','def','abcd'])) // 1
 console.log(countConstruct('skateboard',['bo','rd','ate','t','ska','sk','boar'])) // 0
 console.log(countConstruct('menu',['m','en','nu', 'u'])) // 1
 console.log(countConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','ee','eeeee'])) //0
+
+
+
+const count_construct = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(0)
+  table[0] = 1
+
+  for (var i = 0; i <= target.length; i++) {
+    if (table[i]) {
+      for (var word of wordBank) {
+        // if the word matches the characters starting at position 1
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] += table[i]
+        }
+      }
+    }
+  }
+  return table[target.length]
+}
+
+console.log('iterative');
+console.log(count_construct('abcdef', ['ab','abc','cd','def','abcd'])) // 1
+console.log(count_construct('skateboard',['bo','rd','ate','t','ska','sk','boar'])) // 0
+console.log(count_construct('menu',['m','en','nu', 'u'])) // 1
+console.log(count_construct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e','ee','eeeee'])) //0
