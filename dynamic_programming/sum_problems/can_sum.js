b09d114a8ba13fade245a66aaf53b8f72f7ee034
andykimchris <andrew.muchiri97@gmail.com>
2021-10-21 11:15:38 +0300

    iterative solution using prefilled array

--- dynamic_programming/sum_problems/can_sum.js
+++ dynamic_programming/sum_problems/can_sum.js
@@ -28,5 +28,25 @@ console.log(canSum(7, [2,3])) // true
 console.log(canSum(7, [5,3,4,7])) // true
 console.log(canSum(7, [2,4])) // false
 console.log(canSum(8, [2,3,5])) //true
-
 console.log(canSum(300, [7,14])) //false
+
+const can_sum = (targetSum, numbers) => {
+  const table = Array(targetSum + 1).fill(false)
+  table[0] = true
+
+  for (var i = 0; i < targetSum; i++) {
+    if (table[i]) {
+      for (var num of numbers) {
+        table[i + num] = true
+      }
+    }
+  }
+  return table[targetSum]
+}
+
+console.log('iterative solution');
+console.log(canSum(5, [2,3])) // true
+console.log(canSum(5, [1,1,4])) // true
+console.log(canSum(5, [2,4])) // false
+console.log(canSum(12, [2,7,5])) //true
+console.log(canSum(100, [7,14])) //false
